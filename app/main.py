fa8f1fd753ed82a17bbdf7f7fe101fc93e7ae066
AJSTO <adam100larczyk@gmail.com>
2023-02-10 21:33:38 +0100

    changes in pyspark application + additional tests

--- app/main.py
+++ app/main.py
@@ -1,38 +1,28 @@
 import json
-from jobs import covid_aggregates # importing job
-# import importlib # this will be used if we will have more than one job to do
-import argparse
+from jobs import covid_aggregates
 from pyspark.sql import SparkSession
-
-
-def _parse_arguments():
-    """ Parse arguments provided by spark-submit commend"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--job", required=True)
-    return parser.parse_args()
 
 
 def main():
     """ Main function excecuted by spark-submit command"""
-    args = _parse_arguments()
-
     with open("config.json", "r") as config_file:
         _config = json.load(config_file)
 
     spark = SparkSession.builder \
-        .config("parentProject", "pyspark-covid") \
+        .config("parentProject", _config.get("biquery_project")) \
         .appName(_config.get("app_name")).getOrCreate()
     covid_aggregates.run_job(spark, _config)
 
-    '''
-    # THIS WILL BE USED IF APP WILL HAVE MORE THAN ONE JOBS:
-    job_module = importlib.import_module(f"jobs.{args.job}")
-    job_module.run_job(spark, _config)
-    '''
 
 if __name__ == "__main__":
     main()
 
 # TO RUN PYSPARK JOB via console:
-#  pytest && spark-submit --jars spark-3.1-bigquery-0.28.0-preview.jar \
-#  --files config.json main.py --job covid_aggregates
+#  pytest && spark-submit --jars bq_con/spark-3.1-bigquery-0.28.0-preview.jar --files config.json main.py --job covid_aggregates
+
+'''
+to create dataset in bigquery:
+date string nullable
+unit_id required
+sum_of_infections nullable
+'''
