6b3c0131889b1b301bd2a25015c22d3988f452ce
AJSTO <adam100larczyk@gmail.com>
2023-02-19 22:45:44 +0100

    Cosmetic changes in pyspark app

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 import json
+import pyspark
 from jobs import covid_aggregates
 from pyspark.sql import SparkSession
 
 
 def main():
-    """ Main function excecuted by spark-submit command"""
+    """
+    Main function executed by spark-submit command.
+    Creating Spark Session with connection to Bigquery.
+    """
     with open("config.json", "r") as config_file:
         _config = json.load(config_file)
 
     spark = SparkSession.builder \
-        .config("parentProject", _config.get("biquery_project")) \
+        .config("parentProject", _config.get("bigquery_project")) \
         .appName(_config.get("app_name")).getOrCreate()
     covid_aggregates.run_job(spark, _config)
 
@@ -18,11 +22,5 @@ if __name__ == "__main__":
     main()
 
 # TO RUN PYSPARK JOB via console:
-#  pytest && spark-submit --jars bq_con/spark-3.1-bigquery-0.28.0-preview.jar --files config.json main.py --job covid_aggregates
+#  pytest && spark-submit --jars bigquery_connection/spark-3.1-bigquery-0.28.0-preview.jar --files config.json main.py --job covid_aggregates
 
-'''
-to create dataset in bigquery:
-date string nullable
-unit_id required
-sum_of_infections nullable
-'''
--- app/tests/test_aggregation_infections_per_day.py
+++ app/tests/test_aggregation_infections_per_day.py
@@ -1,6 +1,213 @@
+import pytest
+import pandas as pd
+from jobs import covid_aggregates
+from pyspark.sql import SparkSession
+
+
+class TestCovidAggregates:
+
+    @pytest.fixture
+    def spark_session(self):
+        return (
+            SparkSession.builder.appName('testing_agg').getOrCreate()
+        )
+
+    @pytest.fixture
+    def test_data_fill_zero(self, spark_session):
+        return (
+            spark_session.createDataFrame(
+                [
+                    ('2022-10-11', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 2, 201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                ],
+                [
+                    "data_rap_zakazenia", "teryt_woj", "teryt_pow", "plec", "kat_wiek", "producent",
+                    "dawka_ost", "numer_zarazenia", "odl_szczep_zar", "liczba_zaraportowanych_zakazonych",
+                ],
+            )
+        )
+
+    @pytest.fixture
+    def test_data_without_fulfilling_zeros(self, spark_session):
+        return (
+            spark_session.createDataFrame(
+                [
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                ],
+                [
+                    "data_rap_zakazenia", "teryt_woj", "teryt_pow", "plec", "kat_wiek", "producent",
+                    "dawka_ost", "numer_zarazenia", "odl_szczep_zar", "liczba_zaraportowanych_zakazonych",
+                ],
+            )
+        )
+
+    @pytest.fixture
+    def test_data_null_value_in_voivodship(self, spark_session):
+        return (
+            spark_session.createDataFrame(
+                [
+                    ('2022-10-11', None, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', None, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                ],
+                [
+                    "data_rap_zakazenia", "teryt_woj", "teryt_pow", "plec", "kat_wiek", "producent",
+                    "dawka_ost", "numer_zarazenia", "odl_szczep_zar", "liczba_zaraportowanych_zakazonych",
+                ],
+            )
+        )
+
+    @pytest.fixture
+    def test_data_null_value_in_county(self, spark_session):
+        return (
+            spark_session.createDataFrame(
+                [
+                    ('2022-10-11', 22, None, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, None, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                ],
+                [
+                    "data_rap_zakazenia", "teryt_woj", "teryt_pow", "plec", "kat_wiek", "producent",
+                    "dawka_ost", "numer_zarazenia", "odl_szczep_zar", "liczba_zaraportowanych_zakazonych",
+                ],
+            )
+        )
+
+    @pytest.fixture
+    def test_data_empty_date(self, spark_session):
+        return (
+            spark_session.createDataFrame(
+                [
+                    (None, 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-11', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                    ('2022-10-12', 22, 2201, 'K', '55-64', 'Pfizer', 'przypominająca', 2, '180-270', 1),
+                ],
+                [
+                    "data_rap_zakazenia", "teryt_woj", "teryt_pow", "plec", "kat_wiek", "producent",
+                    "dawka_ost", "numer_zarazenia", "odl_szczep_zar", "liczba_zaraportowanych_zakazonych",
+                ],
+            )
+        )
+
+    def test_aggregation_fill_zero_in_unit_id(self, spark_session, test_data_fill_zero):
+        expected_data = spark_session.createDataFrame(
+            [
+                ("11-10-2022", '02', 3),
+                ("12-10-2022", '02', 3),
+                ("11-10-2022", '0201', 3),
+                ("12-10-2022", '0201', 3)
+            ],
+            [
+                "date", "unit_id", "sum_of_infections"
+            ],
+        ).toPandas()
+
+        real_data = covid_aggregates._infections_per_day_agg(test_data_fill_zero).toPandas()
+
+        pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+
+        spark_session.stop()
+
+    def test_aggregation_without_filling_unit_id(self, spark_session, test_data_without_fulfilling_zeros):
+        expected_data = spark_session.createDataFrame(
+            [
+                ("11-10-2022", '22', 3),
+                ("12-10-2022", '22', 3),
+                ("11-10-2022", '2201', 3),
+                ("12-10-2022", '2201', 3)
+            ],
+            [
+                "date", "unit_id", "sum_of_infections"
+            ],
+        ).toPandas()
+
+        real_data = covid_aggregates._infections_per_day_agg(test_data_without_fulfilling_zeros).toPandas()
+
+        pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+
+        spark_session.stop()
+
+    def test_aggregation_null_value_in_voivodship_id(self, spark_session, test_data_null_value_in_voivodship):
+        expected_data = spark_session.createDataFrame(
+            [
+                ("11-10-2022", '22', 1),
+                ("12-10-2022", '22', 3),
+                ("11-10-2022", '2201', 3),
+                ("12-10-2022", '2201', 3)
+            ],
+            [
+                "date", "unit_id", "sum_of_infections"
+            ],
+        ).toPandas()
+
+        real_data = covid_aggregates._infections_per_day_agg(test_data_null_value_in_voivodship).toPandas()
+
+        pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+
+        spark_session.stop()
+
+    def test_aggregation_null_value_in_county_id(self, spark_session, test_data_null_value_in_county):
+        expected_data = spark_session.createDataFrame(
+            [
+                ("11-10-2022", '22', 3),
+                ("12-10-2022", '22', 3),
+                ("11-10-2022", '2201', 2),
+                ("12-10-2022", '2201', 2)
+            ],
+            [
+                "date", "unit_id", "sum_of_infections"
+            ],
+        ).toPandas()
+
+        real_data = covid_aggregates._infections_per_day_agg(test_data_null_value_in_county).toPandas()
+
+        pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+
+        spark_session.stop()
+
+    def test_aggregation_empty_date(self, spark_session, test_data_empty_date):
+        expected_data = spark_session.createDataFrame(
+            [
+                ("11-10-2022", '22', 2),
+                ("12-10-2022", '22', 3),
+                ("11-10-2022", '2201', 2),
+                ("12-10-2022", '2201', 3)
+            ],
+            [
+                "date", "unit_id", "sum_of_infections"
+            ],
+        ).toPandas()
+
+        real_data = covid_aggregates._infections_per_day_agg(test_data_empty_date).toPandas()
+
+        pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+
+        spark_session.stop()
+
+# pytest --cov jobs.covid_aggregates --cov-report html
+
+'''
 from jobs import covid_aggregates
 import pandas as pd
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkS ession
 
 
 class TestCovidAggregates:
@@ -163,3 +370,4 @@ class TestCovidAggregates:
         real_data = covid_aggregates._infections_per_day_agg(test_data).toPandas()
 
         pd.testing.assert_frame_equal(real_data, expected_data, check_dtype=True)
+'''
